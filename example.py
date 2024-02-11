6733e183539da5e6b0d2a179313642959a00eb79
tehZevo <tehzevo@users.noreply.github.com>
2024-02-11 00:29:53 -0500

    Add multiple pad support

--- example.py
+++ example.py
@@ -1,14 +1,4 @@
-import time
-
-import mido
-from mido import Message
-#https://mido.readthedocs.io/en/stable/ports/index.html#callbacks
-#https://fael-downloads-prod.focusrite.com/customer/prod/s3fs-public/downloads/Launchpad%20X%20-%20Programmers%20Reference%20Manual.pdf
-
 from lunchbox import Lunchbox
-
-print(mido.get_input_names())
-print(mido.get_output_names())
 
 def press(x, y, velocity, pad=0):
     print("pad", pad, "pressed", x, y, velocity)
@@ -17,30 +7,37 @@ def release(x, y, pad=0):
     print("pad", pad, "released", x, y)
 
 def polytouch(x, y, value, pad=0):
+    lunch.light(x, y, value, value, value, pad=pad)
     print("pad", pad, "polytouch", x, y, value)
 
 
-in_device = "MIDIIN2 (LPX MIDI) 3"
-out_device = "MIDIOUT2 (LPX MIDI) 4"
+in_devices = [
+    "MIDIIN2 (LPX MIDI) 1",
+    "MIDIIN4 (LPX MIDI) 3",
+]
+out_devices = [
+    "MIDIOUT2 (LPX MIDI) 2",
+    "MIDIOUT4 (LPX MIDI) 4",
+]
 
-lunch = Lunchbox([in_device], [out_device], press, release, polytouch)
+lunch = Lunchbox(in_devices, out_devices, press, release, polytouch)
 lunch.list_devices()
 lunch.connect()
 
-for r in range(4):
-    for g in range(4):
-        for b in range(4):
-            bx = b % 2
-            by = b // 2
-            x = r + bx * 4
-            y = g + by * 4
-            print(r, g, b, x, y)
-            r2 = r / 4 * 255
-            g2 = g / 4 * 255
-            b2 = b / 4 * 255
-            lunch.light(x, y, r2, g2, b2)
-
-#light up logo
-lunch.light(8, 8, 0, 255, 0)
+# for r in range(4):
+#     for g in range(4):
+#         for b in range(4):
+#             bx = b % 2
+#             by = b // 2
+#             x = r + bx * 4
+#             y = g + by * 4
+#             print(r, g, b, x, y)
+#             r2 = r / 4 * 255
+#             g2 = g / 4 * 255
+#             b2 = b / 4 * 255
+#             lunch.light(x, y, r2, g2, b2)
+# 
+# #light up logo
+# lunch.light(8, 8, 0, 255, 0)
 
 lunch.wait()
--- lunchbox/lunchbox.py
+++ lunchbox/lunchbox.py
@@ -3,8 +3,14 @@ import time
 import mido
 from mido import Message
 
+#https://mido.readthedocs.io/en/stable/ports/index.html#callbacks
+#https://fael-downloads-prod.focusrite.com/customer/prod/s3fs-public/downloads/Launchpad%20X%20-%20Programmers%20Reference%20Manual.pdf
+
 PROGRAMMER_MODE = 1
 LIVE_MODE = 0
+
+LIGHT_SYSEX = [0, 32, 41, 2, 12, 3]
+RGB_LIGHT_TYPE = 3
 
 def note_to_xy(note):
     x = (note % 10) - 1
@@ -22,8 +28,6 @@ def light_message(x, y, r, g, b):
     g = round_color(g)
     b = round_color(b)
     index = xy_to_note(x, y)
-    LIGHT_SYSEX = [0, 32, 41, 2, 12, 3]
-    RGB_LIGHT_TYPE = 3
     return Message("sysex", data=[*LIGHT_SYSEX, RGB_LIGHT_TYPE, index, r, g, b])
 
 
@@ -32,19 +36,20 @@ class Lunchbox:
     def __init__(self, in_devices=[], out_devices=[], on_press=lambda x, y, vel:None, on_release=lambda x, y:None, on_polytouch=lambda x, y, val:None):
         self.in_devices = in_devices
         self.out_devices = out_devices
-        #TODO: make these support a pad number
         self.on_press = on_press
         self.on_release = on_release
         self.on_polytouch = on_polytouch
+        self.in_ports = []
+        self.out_ports = []
     
     def light(self, x, y, r, g, b, pad=0):
         x = max(0, min(x, 8))
         y = max(0, min(y, 8))
         message = light_message(x, y, r, g, b)
-        self.outport.send(message)
+        self.out_ports[pad].send(message)
         
     def handle_message(self, message, pad):
-        # print(message)
+        print(pad)
         if message.type == "note_on":
             x, y = note_to_xy(message.note)
             if message.velocity > 0:
@@ -70,18 +75,22 @@ class Lunchbox:
         for device in mido.get_output_names():
             print("-", device)
     
-    def connect(self):
-        #TODO: search for all launchpads and autoconnect
-        in_device = self.in_devices[0]
-        out_device = self.out_devices[0]
-        pad = 0 #TODO: remove hardcoded pad number
-        #TODO: support multiple devices here
-        self.inport = mido.open_input(in_device, callback=lambda message: self.handle_message(message, pad))
-        self.outport = mido.open_output(out_device)
+    def connect_to_pad(self, in_device, out_device, pad):
+        in_port = mido.open_input(in_device, callback=lambda message: self.handle_message(message, pad))
+        out_port = mido.open_output(out_device)
         
         enter_programmer_mode = Message("sysex", data=[0, 32, 41, 2, 12, 14, PROGRAMMER_MODE])
         #TODO: auto exit prog mode on sigint
-        self.outport.send(enter_programmer_mode)
+        out_port.send(enter_programmer_mode)
+        
+        return in_port, out_port
+        
+    def connect(self):
+        #TODO: search for all launchpads and autoconnect
+        for pad, (in_device, out_device) in enumerate(zip(self.in_devices, self.out_devices)):
+            in_port, out_port = self.connect_to_pad(in_device, out_device, pad)
+            self.in_ports.append(in_port)
+            self.out_ports.append(out_port)
         
     def wait(self):
         while True:
