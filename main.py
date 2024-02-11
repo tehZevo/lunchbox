b5ac6e9012f12abe922b368f7f80396fda06702c
tehZevo <tehzevo@users.noreply.github.com>
2024-02-10 22:33:26 -0500

    OOPify

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import mido
 from mido import Message
 #https://mido.readthedocs.io/en/stable/ports/index.html#callbacks
@@ -23,36 +25,44 @@ def note_to_xy(note):
     y = (note // 10) - 1
     return x, y
 
-def handle_message(message):
-    # print(message)
-    if message.type == "note_on":
-        x, y = note_to_xy(message.note)
-        if message.velocity > 0:
-            press(x, y, message.velocity)
-        else:
-            release(x, y)
-    if message.type == "polytouch":
-        x, y = note_to_xy(message.note)
-        polytouch(x, y, message.value)
-    if message.type == "control_change":
-        x, y = note_to_xy(message.control)
-        if message.value > 0:
-            press(x, y, message.value)
-        else:
-            release(x, y)
+class Lunchbox:
+    def __init__(self, on_press=lambda x, y, vel:None, on_release=lambda x, y:None, on_polytouch=lambda x, y, val:None):
+        #TODO: make these support a pad number
+        self.on_press = on_press
+        self.on_release = on_release
+        self.on_polytouch = on_polytouch
         
-    
-port = mido.open_input("MIDIIN2 (LPX MIDI) 3", callback=handle_message)
-outport = mido.open_output("MIDIOUT2 (LPX MIDI) 4")
-# port.callback = print_message
+    def handle_message(self, message):
+        # print(message)
+        if message.type == "note_on":
+            x, y = note_to_xy(message.note)
+            if message.velocity > 0:
+                self.on_press(x, y, message.velocity)
+            else:
+                self.on_release(x, y)
+        if message.type == "polytouch":
+            x, y = note_to_xy(message.note)
+            self.on_polytouch(x, y, message.value)
+        if message.type == "control_change":
+            x, y = note_to_xy(message.control)
+            if message.value > 0:
+                self.on_press(x, y, message.value)
+            else:
+                self.on_release(x, y)
+                
+    def connect(self):
+        #TODO: search for all launchpads and autoconnect
+        self.inport = mido.open_input("MIDIIN2 (LPX MIDI) 3", callback=lambda message: self.handle_message(message))
+        self.outport = mido.open_output("MIDIOUT2 (LPX MIDI) 4")
+        
+        enter_programmer_mode = Message("sysex", data=[0, 32, 41, 2, 12, 14, PROGRAMMER_MODE])
+        #TODO: auto exit prog mode on sigint
+        self.outport.send(enter_programmer_mode)
+        
+    def wait(self):
+        while True:
+            time.sleep(1)
 
-#TODO: send sysex to LP to turn it into programmer mode
-enter_programmer_mode = Message("sysex", data=[0, 32, 41, 2, 12, 14, PROGRAMMER_MODE])
-
-#TODO: auto exit prog mode on sigint
-
-outport.send(enter_programmer_mode)
-
-import time
-while True:
-    time.sleep(1)
+lunch = Lunchbox(press, release, polytouch)
+lunch.connect()
+lunch.wait()
